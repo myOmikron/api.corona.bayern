03e11121eb16eeff0347be1a28fbe2d46d9c3af3
myOmikron <accounts@omikron.pw>
2020-03-12 00:39:40 +0100

    Removed unsed fields due to change of data source

--- main/api/models.py
+++ main/api/models.py
@@ -1,24 +1,14 @@
 from django.db import models
-from django.db.models import CharField, IntegerField, ForeignKey
+from django.db.models import CharField, IntegerField, ForeignKey, DateField
 
 
 class Location(models.Model):
-    title = CharField(max_length=200, unique=True)
-    locality = CharField(max_length=200)
-    administrative_area_level_3 = CharField(max_length=200)
-    administrative_area_level_3_short = CharField(max_length=5)
-    administrative_area_level_2 = CharField(max_length=200)
-    administrative_area_level_1 = CharField(max_length=200)
-    administrative_area_level_1_short = CharField(max_length=5)
-    country = CharField(max_length=200)
-    country_short = CharField(max_length=3)
-    lat = models.FloatField()
-    long = models.FloatField()
+    name = CharField(max_length=40, unique=True, default="")
 
 
 class Datum(models.Model):
-    title = CharField(max_length=100)
     infected = IntegerField()
-    deaths = IntegerField()
-    recovered = IntegerField()
+    date = DateField(auto_now=True)
+#    deaths = IntegerField()
+#    recovered = IntegerField()
     location = ForeignKey(Location, on_delete=models.CASCADE)
